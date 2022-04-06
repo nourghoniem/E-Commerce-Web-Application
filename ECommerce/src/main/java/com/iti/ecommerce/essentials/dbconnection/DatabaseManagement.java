fc44096cd2410b01040b11387c3778ecf5b0b70f
EngAbdelrahmanMostafa <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-04-07 01:38:35 +0200

    adding a home page to the application

--- ECommerce/src/main/java/com/iti/ecommerce/essentials/dbconnection/DatabaseManagement.java
+++ ECommerce/src/main/java/com/iti/ecommerce/essentials/dbconnection/DatabaseManagement.java
@@ -25,12 +25,14 @@ import java.time.Month;
  * @author nour
  */
 public class DatabaseManagement {
-      public static String AbsolutePath = new File("").getAbsolutePath();
- 
+
+    public static String AbsolutePath = new File("").getAbsolutePath();
+
     Connection conn;
     Statement stmt;
     PreparedStatement pstmt;
     ArrayList<Customer> customers;
+    ArrayList<Product> products;
     ResultSet rs;
     PreparedStatement pst;
 
@@ -75,11 +77,10 @@ public class DatabaseManagement {
             FileInputStream file_in = new FileInputStream(p.getImage());
             pst.setBinaryStream(5, file_in);
             pst.execute();
-            if(p.getProduct_type().equalsIgnoreCase("Laptop")){
-              pst.setInt(6, 1);
-            }
-            else{
-              pst.setInt(6, 2);
+            if (p.getProduct_type().equalsIgnoreCase("Laptop")) {
+                pst.setInt(6, 1);
+            } else {
+                pst.setInt(6, 2);
             }
             int rows = pst.executeUpdate();
             pst.close();
@@ -89,32 +90,55 @@ public class DatabaseManagement {
         }
     }
 
-    public boolean addCustomer(String fname,String lname,String email,String Password,Date Dob,String address,String phone,String interets,int creditLimit) throws SQLException
-        {
-            boolean isAdded =false; 
-            try {
-            String InsertStatement="insert into users(first_name,last_name,dob,email,password,credit_limit,address,phone_number,interests)"
-                                   + "VALUES(?,?,?,?,?,?,?,?,?)";
-            pstmt=conn.prepareStatement(InsertStatement);
+    public boolean addCustomer(String fname, String lname, String email, String Password, Date Dob, String address, String phone, String interets, int creditLimit) throws SQLException {
+        boolean isAdded = false;
+        try {
+            String InsertStatement = "insert into users(first_name,last_name,dob,email,password,credit_limit,address,phone_number,interests)"
+                    + "VALUES(?,?,?,?,?,?,?,?,?)";
+            pstmt = conn.prepareStatement(InsertStatement);
             pstmt.setString(1, fname);
             pstmt.setString(2, lname);
             pstmt.setString(4, email);
             pstmt.setString(5, Password);
-            pstmt.setDate  (3, Dob);
+            pstmt.setDate(3, Dob);
             pstmt.setString(7, address);
             pstmt.setString(8, phone);
             pstmt.setString(9, interets);
-            pstmt.setInt   (6, creditLimit);
-            int i=pstmt.executeUpdate();
-            System.out.println(i+" records inserted"); 
-            isAdded=true;
-            } catch (SQLException e) {
-             System.out.println(" Exception at adding new users in data base : "+e);
-             isAdded=false;
-            }
-            return isAdded;
+            pstmt.setInt(6, creditLimit);
+            int i = pstmt.executeUpdate();
+            System.out.println(i + " records inserted");
+            isAdded = true;
+        } catch (SQLException e) {
+            System.out.println(" Exception at adding new users in data base : " + e);
+            isAdded = false;
         }
-public Connection getConnection(){
-return conn;
-}
+        return isAdded;
+    }
+
+    public List<Product> getProducts() throws SQLException {
+        products = new ArrayList<Product>();
+        try {
+            stmt = conn.createStatement();
+            String SQL = "SELECT name, price, quantity, description, image, product_type from products;";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                Double price = rs.getDouble("price");
+                Integer quantity = rs.getInt("quantity");
+                String description = rs.getString("description");
+                String image = rs.getString("image");
+                String product_type = rs.getString("product_type");
+                Product product = new Product(name, price, quantity, description, image, product_type);
+                products.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
