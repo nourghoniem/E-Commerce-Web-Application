0b4b8d5eef09dce2421b9076c19529a909613110
nourghoniem <Nour1704021@miuegypt.edu.eg>
2022-05-12 23:43:11 +0200

    initiating api usage

--- AndroidApp/app/src/main/java/com/example/ecommerceandroidapp/ViewProductDetails.java
+++ AndroidApp/app/src/main/java/com/example/ecommerceandroidapp/ViewProductDetails.java
@@ -1,11 +1,21 @@
 package com.example.ecommerceandroidapp;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class ViewProductDetails extends AppCompatActivity {
 
@@ -13,9 +23,29 @@ public class ViewProductDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_product_details);
-
+        String url = "http://192.168.122.1:8080/ECommerce/rest/productAPI/ProductInfo/1";
+        HashMap<String, String> param = new HashMap<>();
+        param.put("param", "1");
+        JSONObject jsonObject = new JSONObject(param);
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("Rest Response", response.toString());
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(20 * 1000, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT ));
+        requestQueue.add(jsonObjectRequest);
     }
 }
