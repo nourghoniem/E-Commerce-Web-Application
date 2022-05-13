d71c2a48816b20b071723d181d76583f2e94dfa9
Abdelrahman <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-05-13 22:32:23 +0200

    fixing Api of registration and login

--- ECommerce/src/main/java/com/iti/ecommerce/essentials/restwebservice/AuthenticationAPI.java
+++ ECommerce/src/main/java/com/iti/ecommerce/essentials/restwebservice/AuthenticationAPI.java
@@ -1,5 +1,8 @@
 package com.iti.ecommerce.essentials.restwebservice;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.iti.ecommerce.essentials.dbconnection.DatabaseManagement;
 import org.json.JSONObject;
 
@@ -17,16 +20,16 @@ public class AuthenticationAPI {
 
     @POST
     @Path("/Login")
-  // @Consumes(MediaType.APPLICATION_JSON)
-    @Produces("application/json")
-    public String verifyLogin(
-            @FormParam("email") String email,
-            @FormParam("password") String password)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String verifyLogin(String credintials)
             throws SQLException {
         DatabaseManagement DM = new DatabaseManagement();
-        System.out.println(email);
-        System.out.println(password);
-        return DM.verfiyLoginForREST(email, password);
+        Gson gson = new Gson();
+       CredentialsForLogin Clogin=gson.fromJson(credintials,CredentialsForLogin.class);
+        System.out.println(Clogin.email);
+        System.out.println(Clogin.password);
+        return DM.verfiyLoginForREST(Clogin.email, Clogin.password);
     }
 
     @PUT
@@ -34,24 +37,54 @@ public class AuthenticationAPI {
   //  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces("application/json")
     public String RegisterData(
-            @FormParam("first_name") String first_name,
-            @FormParam("last_name") String last_name,
-            @FormParam("dob") String dob,
-            @FormParam("email") String email,
-            @FormParam("password") String password,
-            @FormParam("credit_limit") int credit_limit,
-            @FormParam("address") String address,
-            @FormParam("phone_number") String phone_number,
-            @FormParam("interests") String interests
+         String Credentials
     ) throws SQLException, ParseException {
-        String SDate = dob;
-        java.util.Date date = new SimpleDateFormat("yyyy-MM-dd").parse(SDate);
-        java.sql.Date DoB = new java.sql.Date(date.getTime());
         DatabaseManagement DM = new DatabaseManagement();
-        System.out.println(email);
-        System.out.println(password);
-        String checkRegister = String.valueOf(DM.addCustomer(first_name, last_name, email, password, DoB, address, phone_number, interests, credit_limit));
+        Gson gson = new Gson();
+        CredentialsForRegistration CRegister = gson.fromJson(Credentials, CredentialsForRegistration.class);
+        System.out.println(CRegister.email);
+      //  System.out.println(password);
+        String checkRegister = String.valueOf(DM.addCustomer(CRegister.first_name, CRegister.last_name, CRegister.email, CRegister.password, CRegister.dob, CRegister.address, CRegister.phone_number, CRegister.interests, CRegister.credit_limit));
         JSONObject json=new JSONObject();
         return json.put("register",checkRegister).toString();
     }
 }
+class CredentialsForLogin {
+    String email;
+    String password;
+
+    public CredentialsForLogin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
+    class  CredentialsForRegistration{
+        String first_name;
+        String last_name;
+        java.sql.Date dob;
+        String Date_of_birth;
+        String email;
+        String password;
+        int credit_limit;
+        String address;
+        String phone_number;
+        String interests;
+
+        public CredentialsForRegistration(String first_name, String last_name, String dob, String email, String password, int credit_limit, String address, String phone_number, String interests) throws ParseException {
+
+            this.first_name = first_name;
+            this.last_name = last_name;
+      //      this.dob = new java.sql.Date(new SimpleDateFormat("yyyy-MM-dd").parse(dob).getTime());
+
+            this.Date_of_birth=dob;
+            this.email = email;
+            this.password = password;
+            this.credit_limit = credit_limit;
+            this.address = address;
+            this.phone_number = phone_number;
+            this.interests = interests;
+
+            this.dob= new java.sql.Date(new SimpleDateFormat("dd-MM-yyyy").parse(dob).getTime());
+
+        }
+    }
