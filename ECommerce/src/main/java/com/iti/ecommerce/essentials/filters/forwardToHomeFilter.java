52926ed94e306407c53b4d8fd18397a755f20cd3
Abdelrahman <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-05-13 23:43:11 +0200

    home filter

---
+++ ECommerce/src/main/java/com/iti/ecommerce/essentials/filters/forwardToHomeFilter.java
@@ -0,0 +1,26 @@
+package com.iti.ecommerce.essentials.filters;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class forwardToHomeFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.sendRedirect("/ECommerce/JSPfiles/home.jsp");
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
